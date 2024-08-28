72aca533e67cd961f0391a8d79418648b521a151
fazal <fazalahamed628@gmail.com>
2024-08-28 15:48:03 +0530

    fix:bug fix

--- src/utils/errorboundary/errorBoundary.tsx
+++ src/utils/errorboundary/errorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import  { Component, ErrorInfo, ReactNode } from 'react';
 import { toast } from 'react-toastify';
 import { AxiosError } from 'axios';
 
