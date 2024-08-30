df7682ef99b9b01e567df2ecd7d41518be58cc52
fazal <fazalahamed628@gmail.com>
2024-08-30 14:09:55 +0530

    fix:Socket layer fixed

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -25,7 +25,7 @@ export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({ childr
 
   const initializeSocket = (userId: string) => {
     if (!socketRef.current) {
-      const newSocket = io('www.thecoffeeland.shop', {
+      const newSocket = io('https://www.thecoffeeland.shop', {
         autoConnect: false,
         transports: ['websocket'],
         query: { userId },
--- src/hooks/useSocketNotification.ts
+++ src/hooks/useSocketNotification.ts
@@ -21,7 +21,7 @@ export const useSocketNotification = (userId: string) => {
   const [isReject,setIsReject]=useState(false)
 
   useEffect(() => {
-    const newSocket = io('www.thecoffeeland.shop');
+    const newSocket = io('https://www.thecoffeeland.shop');
     newSocket.on('connect', () => {
       console.log('Connected to socket server with ID:', newSocket.id);
       newSocket.emit('joinRoom', userId);
